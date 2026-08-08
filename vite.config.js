c9f3e5ad59840751f79e0ce8eace7b01c9175f08
Pravin Nalla <pravinnalla10@gmail.com>
2026-08-08 20:21:44 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+
+    base: "/product-catalog/",
+
     server: {
+
         port: 5173,
+
         open: true
+
     },
 
     build: {
+
         outDir: "dist",
+
         emptyOutDir: true
+
     }
+
 });
